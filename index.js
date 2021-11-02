e3477e02f9c6b61a15003fdddfb066e346621e0d
Bernard Mulaw <bmulaw@bu.edu>
2021-11-01 21:37:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function getName(){
     a = document.getElementById("word");
     console.log(a.value);
+    b = getNewWord();
+    document.getElementById("word").innerHTML = b;
+}
+
+function getNewWord() {
+    return "new word is returned";
 }
